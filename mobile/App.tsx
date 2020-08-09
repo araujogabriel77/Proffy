0ae2a030ebf6c2009297cc47b1ae4885d8de5d01
araujogabriel77 <araujogabrielocn@gmail.com>
2020-08-09 00:18:35 -0300

    Finalizando dia 4

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { AppLoading } from 'expo';
+import AppStack from './src/routes/AppStack';
+
+//fonts
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
-import Landing from './src/pages/Landing';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -20,7 +22,7 @@ export default function App() {
   } else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
     );
